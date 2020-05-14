1ee6cec128cea669809839f09dffd8cb4d94419f
Yettsy Knapp <yettsyknapp@SDMacBookPro3-2.local>
2020-05-14 16:31:39 -0600

    html js setup

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,8 @@
 const convertFile = new showdown.Converter({simplifiedAutoLink: true})
+const pdf = new jsPDF();
+
+convertFile.getOption({simplifiedAutoLink: true})
+
+$('#editor').keyup(function() {
+  $('#output').html(converter.makeHtml($(this).val()))
+})
