a7910a7ceb39ce298b1e3f09dcbbe6d74392e718
Yettsy Knapp <yettsyknapp@SDMacBookPro3-2.local>
2020-05-14 19:57:53 -0600

    markdown issues

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,10 @@ convertFile.getOption({simplifiedAutoLink: true})
 $('#editor').keyup(function() {
   $('#output').html(converter.makeHtml($(this).val()))
 })
+
+
+$('#export').click(async function() {
+  htmlToPdf($('#output').html(), {
+    filename: $('#fiename').val() || 'markdown', margin: 10
+  })
+})
